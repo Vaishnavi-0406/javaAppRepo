89bf0c77a7c3e1d2ab0f047d80506fb9617f46c7
Vaishnavi-0406 <panduuppugalla@gmail.com>
2024-11-27 14:08:35 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,8 @@
+class Demo{
+      public static void main(string[] args){
+         int x=5;
+         int y=6;
+         system.out.println(x*y);
+         system.out.println("ci/cd pipeline using java programming language")
+      }
+}
